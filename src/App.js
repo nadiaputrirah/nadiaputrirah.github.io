2b74a4f2a877039314f0bc564f88bc7af2427566
nadiaputrirah <nadiapputriirah@gmail.com>
2024-06-09 12:20:47 +0700

    Update Router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import {BrowserRouter as Router, Route, Routes, useLocation, useNavigate} from "react-router-dom";
-import { useEffect } from "react";
-import "./App.css";
-
+import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/index";
 import Project from "./pages/Project/project";
 import Skills from "./pages/Skills/skills";
 import Certificate from "./pages/Certificate/certificate";
 import Contact from "./pages/Contact/contact";
 
-function App() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (location.pathname === "/") {
-      navigate("/home");
-    }
-  }, [location, navigate]);
-
+const Router = () => {
   return (
-    <Routes>
-      <Route path="/home/*" element={<Home />} />
-      <Route path="/project/*" element={<Project />} />
-      <Route path="/skills/*" element={<Skills />} />
-      <Route path="/certificate/*" element={<Certificate />} />
-      <Route path="/contact/*" element={<Contact />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/certificate" element={<Certificate />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
   );
-}
+};
 
-export default App;
+export default Router;
